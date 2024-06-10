9f2170e136890ae70ed7a09ebd3b6dd92b1f795f
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-06-10 16:23:53 +0530

    10-6-24 Updated

---
+++ ListSlice.py
@@ -0,0 +1,10 @@
+l=[1,2,3,1.1,2.2,"tops",True,"python","java",10,20,30,False,5,6,7]
+
+print(l[4:5:4])
+print(l[:5:10])
+print(l[9::9])
+print(l[::10])
+print(l[-16::10])
+print(l[-14:-4:4])
+print(l[:-1:10])
+print(l[::-5])
